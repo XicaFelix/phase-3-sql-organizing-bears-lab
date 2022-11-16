b468fd76a0b55ea8fd6a1511c32d3d186191a050
Xica Felix <xicasfelix@gmail.com>
2022-11-16 11:24:30 -0500

    commit tree 1a4015167781877d3b0964908ae752656ca201dd

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'brown', 'sad', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 17, 'F', 'blurple', 'zany', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'hot pink', 'cute', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Seargent Brown', 3, 'M', 'brown', 'sad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'F', 'green pea', 'joyous', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 1, 'M', 'blight yellow', 'green with envy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'F', 'orange', 'perplexed', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 485, 'M', 'rainbow', 'transcendant', 1);
